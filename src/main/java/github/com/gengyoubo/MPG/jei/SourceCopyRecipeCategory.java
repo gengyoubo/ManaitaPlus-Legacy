0ad9050706f4a7fa8d56bb98ecfd0594f4ab0701
gengyoubo <gengyoubo@gmail.com>
2026-04-28 16:35:08 +0900

    2.1.0

--- src/main/java/github/com/gengyoubo/MPG/jei/SourceCopyRecipeCategory.java
+++ src/main/java/github/com/gengyoubo/MPG/jei/SourceCopyRecipeCategory.java
@@ -1,68 +1,68 @@
-package github.com.gengyoubo.MPG.jei;
-
-import github.com.gengyoubo.MPG.core.MPGItemCore;
-import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
-import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
-import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.recipe.IFocusGroup;
-import mezz.jei.api.recipe.RecipeType;
-import mezz.jei.api.recipe.category.IRecipeCategory;
-import mezz.jei.api.recipe.RecipeIngredientRole;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.NotNull;
-
-public class SourceCopyRecipeCategory implements IRecipeCategory<SourceCopyJeiRecipe> {
-    public static final RecipeType<SourceCopyJeiRecipe> TYPE =
-            RecipeType.create("manaita_plus_general", "source_copying", SourceCopyJeiRecipe.class);
-
-    private final IDrawable background;
-    private final IDrawable icon;
-
-    public SourceCopyRecipeCategory(IGuiHelper guiHelper) {
-        this.background = guiHelper.createBlankDrawable(118, 54);
-        this.icon = guiHelper.createDrawableItemStack(new ItemStack(MPGItemCore.ManaitaSource.get()));
-    }
-
-    @Override
-    public @NotNull RecipeType<SourceCopyJeiRecipe> getRecipeType() {
-        return TYPE;
-    }
-
-    @Override
-    public @NotNull Component getTitle() {
-        return Component.translatable("jei.manaita_plus_general.source_copying");
-    }
-
-    @Override
-    public @NotNull IDrawable getBackground() {
-        return background;
-    }
-
-    @Override
-    public @NotNull IDrawable getIcon() {
-        return icon;
-    }
-
-    @Override
-    public void setRecipe(@NotNull IRecipeLayoutBuilder builder, @NotNull SourceCopyJeiRecipe recipe, @NotNull IFocusGroup focuses) {
-        builder.addSlot(RecipeIngredientRole.INPUT, 4, 18).addItemStack(recipe.source());
-        builder.addSlot(RecipeIngredientRole.INPUT, 40, 18).addItemStack(recipe.input());
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 94, 18).addItemStack(recipe.output());
-    }
-
-    @Override
-    public void draw(@NotNull SourceCopyJeiRecipe recipe, @NotNull IRecipeSlotsView recipeSlotsView, @NotNull GuiGraphics guiGraphics, double mouseX, double mouseY) {
-        guiGraphics.drawString(net.minecraft.client.Minecraft.getInstance().font, ">", 72, 22, 0x404040, false);
-        guiGraphics.drawString(
-                net.minecraft.client.Minecraft.getInstance().font,
-                Component.translatable("jei.manaita_plus_general.source.info.2", recipe.output().getCount()),
-                4,
-                42,
-                0x404040,
-                false
-        );
-    }
-}
+//package github.com.gengyoubo.MPG.jei;
+//
+//import github.com.gengyoubo.MPG.core.MPGItemCore;
+//import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+//import mezz.jei.api.gui.drawable.IDrawable;
+//import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+//import mezz.jei.api.helpers.IGuiHelper;
+//import mezz.jei.api.recipe.IFocusGroup;
+//import mezz.jei.api.recipe.RecipeType;
+//import mezz.jei.api.recipe.category.IRecipeCategory;
+//import mezz.jei.api.recipe.RecipeIngredientRole;
+//import net.minecraft.client.gui.GuiGraphics;
+//import net.minecraft.network.chat.Component;
+//import net.minecraft.world.item.ItemStack;
+//import org.jetbrains.annotations.NotNull;
+//
+//public class SourceCopyRecipeCategory implements IRecipeCategory<SourceCopyJeiRecipe> {
+//    public static final RecipeType<SourceCopyJeiRecipe> TYPE =
+//            RecipeType.create("manaita_plus_general", "source_copying", SourceCopyJeiRecipe.class);
+//
+//    private final IDrawable background;
+//    private final IDrawable icon;
+//
+//    public SourceCopyRecipeCategory(IGuiHelper guiHelper) {
+//        this.background = guiHelper.createBlankDrawable(118, 54);
+//        this.icon = guiHelper.createDrawableItemStack(new ItemStack(MPGItemCore.ManaitaSource.get()));
+//    }
+//
+//    @Override
+//    public @NotNull RecipeType<SourceCopyJeiRecipe> getRecipeType() {
+//        return TYPE;
+//    }
+//
+//    @Override
+//    public @NotNull Component getTitle() {
+//        return Component.translatable("jei.manaita_plus_general.source_copying");
+//    }
+//
+//    @Override
+//    public @NotNull IDrawable getBackground() {
+//        return background;
+//    }
+//
+//    @Override
+//    public @NotNull IDrawable getIcon() {
+//        return icon;
+//    }
+//
+//    @Override
+//    public void setRecipe(@NotNull IRecipeLayoutBuilder builder, @NotNull SourceCopyJeiRecipe recipe, @NotNull IFocusGroup focuses) {
+//        builder.addSlot(RecipeIngredientRole.INPUT, 4, 18).addItemStack(recipe.source());
+//        builder.addSlot(RecipeIngredientRole.INPUT, 40, 18).addItemStack(recipe.input());
+//        builder.addSlot(RecipeIngredientRole.OUTPUT, 94, 18).addItemStack(recipe.output());
+//    }
+//
+//    @Override
+//    public void draw(@NotNull SourceCopyJeiRecipe recipe, @NotNull IRecipeSlotsView recipeSlotsView, @NotNull GuiGraphics guiGraphics, double mouseX, double mouseY) {
+//        guiGraphics.drawString(net.minecraft.client.Minecraft.getInstance().font, ">", 72, 22, 0x404040, false);
+//        guiGraphics.drawString(
+//                net.minecraft.client.Minecraft.getInstance().font,
+//                Component.translatable("jei.manaita_plus_general.source.info.2", recipe.output().getCount()),
+//                4,
+//                42,
+//                0x404040,
+//                false
+//        );
+//    }
+//}

5fdc8015d48feb24ac86690c8563d792e2a5028a
耿悠博 <113955003+gengyoubo@users.noreply.github.com>
2026-05-08 09:20:09 +0900

    Update RegisterEventHandler.java

--- src/main/java/github/com/gengyoubo/MPG/event/RegisterEventHandler.java
+++ src/main/java/github/com/gengyoubo/MPG/event/RegisterEventHandler.java
@@ -4,22 +4,21 @@ import com.mojang.blaze3d.platform.InputConstants;
 import github.com.gengyoubo.MPG.MPG;
 import github.com.gengyoubo.MPG.core.*;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.client.renderer.item.ItemPropertyFunction;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.client.settings.KeyConflictContext;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import github.com.gengyoubo.MPG.gui.BrewingStandScreen;
-import github.com.gengyoubo.MPG.gui.BaublesScreen;
 import github.com.gengyoubo.MPG.gui.CraftingManaitaScreen;
 import github.com.gengyoubo.MPG.gui.FurnaceManaitaScreen;
 import github.com.gengyoubo.MPG.blockEntity.RenderBrewingManaitaBlockEntity;
@@ -34,16 +33,12 @@ import github.com.gengyoubo.MPG.util.MPGNBTData;
 import static github.com.gengyoubo.MPG.core.MPGEntityCore.ManaitaArrow;
 import static github.com.gengyoubo.MPG.core.MPGEntityCore.ManaitaLightningBolt;
 
-@Mod.EventBusSubscriber(modid = MPG.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = MPG.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class RegisterEventHandler {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             ClientEventHandler.register();
-            MenuScreens.register(MPGMenuCore.CraftingManaita.get(), CraftingManaitaScreen::new);
-            MenuScreens.register(MPGMenuCore.FurnaceManaita.get(), FurnaceManaitaScreen::new);
-            MenuScreens.register(MPGMenuCore.BrewingStandManaita.get(), BrewingStandScreen::new);
-            MenuScreens.register(MPGMenuCore.Baubles.get(), BaublesScreen::new);
             acceptTypePropertyFunction(
                     MPGBlockCore.CraftingBlockItem.get(),
                     MPGBlockCore.FurnaceBlockItem.get(),
@@ -60,6 +55,13 @@ public class RegisterEventHandler {
     }
 
     @SubscribeEvent
+    public static void onRegisterMenuScreens(RegisterMenuScreensEvent event) {
+        event.register(MPGMenuCore.CraftingManaita.get(), CraftingManaitaScreen::new);
+        event.register(MPGMenuCore.FurnaceManaita.get(), FurnaceManaitaScreen::new);
+        event.register(MPGMenuCore.BrewingStandManaita.get(), BrewingStandScreen::new);
+    }
+
+    @SubscribeEvent
     public static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(ManaitaLightningBolt.get(), MPLightningBoltRenderer::new);
         event.registerEntityRenderer(ManaitaArrow.get(), RenderManaitaArrow::new);
@@ -70,14 +72,17 @@ public class RegisterEventHandler {
     }
 
     @SubscribeEvent
+    public static void onRegisterClientExtensions(RegisterClientExtensionsEvent event) {
+        event.registerItem(MPGGodSwordItem.CLIENT_EXTENSIONS, MPGItemCore.ManaitaSwordGod.get());
+    }
+
+    @SubscribeEvent
     public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event)
     {
         MPGKeyBoardCore.MESSAGE_KEY = new KeyMapping("key.manaita", KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, 88, "key.categories.misc");
         MPGKeyBoardCore.MESSAGE_ARMOR_KEY = new KeyMapping("key.manaita.armor", KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, 89, "key.categories.misc");
-        MPGKeyBoardCore.BAUBLES_KEY = new KeyMapping("key.manaita.baubles", KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, 66, "key.categories.inventory");
         event.register(MPGKeyBoardCore.MESSAGE_KEY);
         event.register(MPGKeyBoardCore.MESSAGE_ARMOR_KEY);
-        event.register(MPGKeyBoardCore.BAUBLES_KEY);
     }
 
     @SuppressWarnings("deprecation")
@@ -90,3 +95,4 @@ public class RegisterEventHandler {
         }
     }
 }
+
